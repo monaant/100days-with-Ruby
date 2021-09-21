809e8ce1da80836f8671629231a1a02ac1052276
monaant <79448034+monaant@users.noreply.github.com>
2021-09-21 13:00:51 +0200

    dodaje wszystkie pliki z 1 dnia ćwiczeń

---
+++ 100 dni z Ruby/1st day-2.rb
@@ -0,0 +1,4 @@
+puts "Hello! Could you give me your favorite number?"
+number = gets.chomp.to_i
+
+puts "#{number + 1}" + " is better. As you think?"
---
+++ 100 dni z Ruby/1st day-3.rb
@@ -0,0 +1,4 @@
+puts 'Angry boss ask: "What do you want?"'
+answer = gets.chomp
+
+puts 'WHADDAYA MEAN ' + "'" "#{answer}".upcase + "'?!?" + " YOU'RE FIRED!!"
